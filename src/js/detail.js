35f3b8ded3196e4f678e9aac1281970e0cbb56df
chenxuanfeng <chenxuanfeng@chenxuanfengdeMacBook-Pro.local>
2017-10-16 20:20:57 +0800

    删除废弃代码

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,11 +1,6 @@
 import detail from '../style/css/detail.less';
 import print from '../lib/printMsg.js';
-const pagename = "菜品详情页!";
-const dateTime = Date.now();
-const {ua,language} = {ua:navigator.userAgent,language:navigator.language};
-const log = text => {console.log(text);}
-console.log(pagename + ' : ' + dateTime);
-console.log(ua);
+print.log('ua: ' + navigator.userAgent);
 print.log('详情页');
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import Styles from '../style/css/index.less';
 import print from '../lib/printMsg.js';
-console.log('首页打包！');
 print.log('首页');
 
 
